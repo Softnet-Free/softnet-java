fbd3e4b33dc943ca4d53ae8df2d8f57a7310809c
Robert Koifman <robert.koifman@gmail.com>
2023-08-25 20:13:23 +0500

    ...

--- src/softnet/ConnectionMode.java
+++ src/softnet/ConnectionMode.java
@@ -1,22 +1,3 @@
-/*
-*	Copyright (c) 2023 Robert Koifman 
-*	
-*	This file is part of Softnet (Java).
-*	
-*	Softnet (Java) is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
-*	
-*	Softnet (Java) is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*	GNU General Public License for more details.
-*	
-*	You should have received a copy of the GNU General Public License
-*	along with Softnet (Java). If not, see <https://www.gnu.org/licenses/>.
-*/
-
 package softnet;
 
 public enum ConnectionMode {
